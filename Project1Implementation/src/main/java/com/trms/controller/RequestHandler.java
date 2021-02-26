9a1ea841564ad9886832d2fe99857c46937cec92
roger_moore <rogermoore06@gmail.com>
2021-02-26 01:40:35 -0600

    commiting

--- Project1Implementation/src/main/java/com/trms/controller/RequestHandler.java
+++ Project1Implementation/src/main/java/com/trms/controller/RequestHandler.java
@@ -22,9 +22,11 @@ public class RequestHandler {
 	
 	{
 		delegateMap = new HashMap<String, FrontControllerDelegate>();
-		
+		//this is /employee and /login
 		delegateMap.put("employee", new EmployeeDelegate());
+		delegateMap.put("employee2", new EmployeeDelegate());
 		delegateMap.put("login", new LoginDelegate());
+		delegateMap.put("login2", new LoginDelegate());
 	}
 	
 	public FrontControllerDelegate handle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
