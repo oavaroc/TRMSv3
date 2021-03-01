408c1b6e2e4d385d37f399d085b1045b4518fbd7
roger_moore <rogermoore06@gmail.com>
2021-03-01 06:06:26 -0600

    commiting

--- Project1Implementation/src/main/java/com/trms/delegates/LoginDelegate.java
+++ Project1Implementation/src/main/java/com/trms/delegates/LoginDelegate.java
@@ -20,77 +20,87 @@ import com.trms.services.EmployeeServiceImpl;
  *            - (PUT) update a user
  *            - (DELETE) deletes a user
  */
-public class LoginDelegate implements FrontControllerDelegate{
+public class LoginDelegate implements FrontControllerDelegate {
 	private EmployeeService es = new EmployeeServiceImpl();
 	private ObjectMapper om = new ObjectMapper();
+
 	@Override
 	public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		String path = (String) req.getAttribute("path");
-//		
-//		if (path == null || path.equals("")) {
-//			if ("POST".equals(req.getMethod())) {
-//				Employee e = (Employee) om.readValue(req.getInputStream(),Employee.class);
-//				
-//			}
-//		}else if (path.contains("login")) {
-//			if ("POST".equals(req.getMethod()))
-//				logIn(req, resp);
-//			else if ("DELETE".equals(req.getMethod()))
-//				req.getSession().invalidate();
-//			else
-//				resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-//		} else {
-//			//userWithId(req, resp, Integer.valueOf(path));
-//		}
-		Employee e = new Employee();
-		e.setFirstName("string");
-		e.setUsername(req.getParameter("user"));
-		e.setPass(req.getParameter("pass"));
-		resp.getWriter().write(om.writeValueAsString(e));
+		String path = (String) req.getAttribute("path");
+
+		if (path == null || path.equals("")) {
+			if ("POST".equals(req.getMethod())) {
+				// register a user
+				Employee p = (Employee) om.readValue(req.getInputStream(), Employee.class);
+				try {
+					p.setId(es.addEmployee(p));
+				} catch (Exception e) {
+					resp.sendError(HttpServletResponse.SC_CONFLICT, "Username already exists");
+				}
+				if (p.getId() == 0) {
+					resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				} else {
+					resp.getWriter().write(om.writeValueAsString(p));
+					resp.setStatus(HttpServletResponse.SC_CREATED);
+				}
+			} else {
+				resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			}
+		} else if (path.contains("login")) {
+			if ("POST".equals(req.getMethod()))
+				logIn(req, resp);
+			else if ("DELETE".equals(req.getMethod()))
+				req.getSession().invalidate();
+			else
+				resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+		} else {
+			userWithId(req, resp, Integer.valueOf(path));
+		}
 	}
-//	private void logIn(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//		String username = req.getParameter("user");
-//		String password = req.getParameter("pass");
-//		
-////		Employee p = es.getPersonByUsername(username);
-////		if (p != null) {
-////			if (p.getPassword().equals(password)) {
-////				req.getSession().setAttribute("person", p);
-////				resp.getWriter().write(om.writeValueAsString(p));
-////			} else {
-////				resp.sendError(404, "Incorrect password.");
-////			}
-////		} else {
-////			resp.sendError(404, "No user found with that username.");
-////		}
-//		resp.getWriter().write(om.writeValueAsString(username));
-//	}
-//	private void userWithId(HttpServletRequest req, HttpServletResponse resp, Integer id) throws IOException {
-//		switch (req.getMethod()) {
-//			case "GET":
-//				Employee p = es.getPersonById(id);
-//				if (p != null) {
-//					resp.getWriter().write(om.writeValueAsString(p));
-//				} else {
-//					resp.sendError(404, "Person not found.");
-//				}
-//				break;
-//			case "PUT":
-//				String password = req.getParameter("pass");
-//				Employee person = (Employee) req.getSession().getAttribute("employee");
-//				if (person != null) {
-//					person.setPassword(password);
-//					es.updatePerson(person);
-//				} else
-//					resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-//				break;
-//			case "DELETE":
-//				Employee user = om.readValue(req.getInputStream(), Employee.class);
-//				es.deletePerson(user);
-//				break;
-//			default:
-//				resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-//				break;
-//		}
-//	}
+
+	private void logIn(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String username = req.getParameter("user");
+		String password = req.getParameter("pass");
+
+		Employee p = es.getEmployeeByUsername(username);
+		if (p != null) {
+			if (p.getPass().equals(password)) {
+				req.getSession().setAttribute("person", p);
+				resp.getWriter().write(om.writeValueAsString(p));
+			} else {
+				resp.sendError(404, "Incorrect password.");
+			}
+		} else {
+			resp.sendError(404, "No user found with that username.");
+		}
+	}
+
+	private void userWithId(HttpServletRequest req, HttpServletResponse resp, Integer id) throws IOException {
+		switch (req.getMethod()) {
+		case "GET":
+			Employee p = es.getEmployeeById(id);
+			if (p != null) {
+				resp.getWriter().write(om.writeValueAsString(p));
+			} else {
+				resp.sendError(404, "Person not found.");
+			}
+			break;
+		case "PUT":
+			String password = req.getParameter("pass");
+			Employee person = (Employee) req.getSession().getAttribute("person");
+			if (person != null) {
+				person.setPass(password);;
+				es.updateEmployee(person);;
+			} else
+				resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+			break;
+		case "DELETE":
+			Employee user = om.readValue(req.getInputStream(), Employee.class);
+			es.deleteEmployee(user);;
+			break;
+		default:
+			resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			break;
+		}
+	}
 }
